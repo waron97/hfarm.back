a83d5b30b7fabb391abdf65cf350e46c0c2c0197
waron97 <waron97@users.noreply.github.com>
2021-03-12 00:32:52 +0100

    initial

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'yz5099+)z!3bbhk=e3-3&-gxphom&2!((yd%&aze&9u%!0$r2p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'hfarm.aronwinkler.com',
                  'candidatura-winkler.herokuapp.com']
@@ -133,6 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/static'),)
